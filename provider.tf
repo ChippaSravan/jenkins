39b7a99879c8a2f17adc4c6d5cae70ddc3317b78
ChippaSravan <85842385+ChippaSravan@users.noreply.github.com>
2024-04-17 01:05:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,5 @@
-terraform {
- required_providers {
-  aws={
-   source="hashicorp/aws"
-   version="5.45.0"
-  }
- }
-}
 provider "aws" {
-  profile ="default"
-  region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   }
